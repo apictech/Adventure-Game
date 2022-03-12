3ae4992e34cd8d635087fe818028ab66d44dcbb0
Zer0-Official <vlethestars10@gmail.com>
2022-03-11 23:06:56 -0600

    fixed text wrapping

--- game.py
+++ game.py
@@ -26,14 +26,11 @@ def restart_game():
 
 
 def intro():
-    print_sleep("You find yourself standing in an open area, "
-                "filled with grass and yellow wildflowers.")
-    print_sleep(f"Rumor has it that a {demon} is somewhere around here, "
-                "and has been terrifying the nearby village.")
+    print_sleep("You find yourself standing in an open area, filled with grass and yellow wildflowers.")
+    print_sleep(f"Rumor has it that a {demon} is somewhere around here, and has been terrifying the nearby village.")
     print_sleep("In front of you is a house.")
     print_sleep("To your right is a dark cave.")
-    print_sleep("In your hand you hold your trusty "
-                "(but not very effective) dagger.\n")
+    print_sleep("In your hand you hold your trusty (but not very effective) dagger.\n")
 
 
 def field():
@@ -54,25 +51,19 @@ def field():
 def house():
     # Things that happen to the player in the house.
     print_sleep("You approach the door of the house.")
-    print_sleep(f"You are about to knock when the "
-                "door opens and out steps a {demon}.")
+    print_sleep(f"You are about to knock when the door opens and out steps a {demon}.")
     print_sleep(f"Eep! This is the {demon}'s house!")
     print_sleep(f"The {demon} attacks you!")
 
     if "sword" in items:
-        print_sleep(f"As the {demon} moves to attack, "
-                    "you unsheath your new sword.")
-        print_sleep("The Sword of Ogoroth shines brightly in "
-                    "your hand as you brace yourself for the attack.")
-        print_sleep(f"But the {demon} takes one look at "
-                    "your shiny new toy and runs away!")
-        print_sleep(f"You have rid the town of "
-                    "the {demon}. You are victorious!")
+        print_sleep(f"As the {demon} moves to attack, you unsheath your new sword.")
+        print_sleep("The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.")
+        print_sleep(f"But the {demon} takes one look at your shiny new toy and runs away!")
+        print_sleep(f"You have rid the town of the {demon}. You are victorious!")
         restart_game()
 
     elif "sword" not in items:
-        print_sleep("You feel a bit under-prepared for "
-                    "this, what with only having a tiny dagger.")
+        print_sleep("You feel a bit under-prepared for this, what with only having a tiny dagger.")
         response = input("Would you like to (1) fight or (2) run away?\n")
 
         if response == '1':
@@ -82,8 +73,7 @@ def house():
             restart_game()
 
         elif response == '2':
-            print_sleep("You run back into the field. Luckily, "
-                        "you don't seem to have been followed.\n")
+            print_sleep("You run back into the field. Luckily, you don't seem to have been followed.\n")
             field()
 
 
@@ -91,8 +81,7 @@ def cave():
     # Things that happen to the player goes in the cave
     if "sword" in items:
         print_sleep("You peer cautiously into the cave.Cave is full of darkness")
-        print_sleep("You've been here before, and gotten all "
-                    "the good stuff. It's just an empty cave now.")
+        print_sleep("You've been here before, and gotten all the good stuff. It's just an empty cave now.")
         print_sleep("You walk back out to the field.")
         field()
     else:
@@ -100,8 +89,7 @@ def cave():
         print_sleep("It turns out to be only a very small cave.")
         print_sleep("Your eye catches a glint of metal behind a rock.")
         print_sleep("You have found the magical Sword of Ogoroth!")
-        print_sleep("You discard your silly old dagger and "
-                    "take the sword with you.")
+        print_sleep("You discard your silly old dagger and take the sword with you.")
         print_sleep("You walk back out to the field.\n")
         items.append("sword")
         field()
